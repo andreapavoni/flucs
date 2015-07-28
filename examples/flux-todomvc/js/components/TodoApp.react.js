8b89de78c47c847c352e3fd06544b4e83d28bad8
Andrea Pavoni <andrea.pavoni@gmail.com>
2015-07-28 12:52:58 +0200

    restore (almost) original TodoApp component from Flux todomvc example

--- examples/flux-todomvc/js/components/TodoApp.react.js
+++ examples/flux-todomvc/js/components/TodoApp.react.js
@@ -1,39 +1,59 @@
-import Footer from './Footer.react'
-import Header from './Header.react'
-import MainSection from './MainSection.react'
-import React from 'react'
-import TodoStore from '../stores/TodoStore'
+/**
+ * Copyright (c) 2014-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 
-export default class TodoApp extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      todos: TodoStore.getState().todos
-    }
-  }
+/**
+ * This component operates as a "Controller-View".  It listens for changes in
+ * the TodoStore and passes the new data to its children.
+ */
 
-  componentDidMount() {
-    TodoStore.addChangeListener(this.onChange.bind(this))
-  }
+var Footer = require('./Footer.react');
+var Header = require('./Header.react');
+var MainSection = require('./MainSection.react');
+var React = require('react');
+var TodoStore = require('../stores/TodoStore');
 
-  componentWillUnmount() {
-    TodoStore.removeChangeListener(this.onChange.bind(this))
-  }
+var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {todos: TodoStore.getState().todos};
+  },
 
-  onChange() {
-    this.setState(TodoStore.getState())
-  }
+  componentDidMount: function() {
+    TodoStore.addChangeListener(this._onChange).bind(this);
+  },
 
-  render() {
-    return (
+  componentWillUnmount: function() {
+    TodoStore.removeChangeListener(this._onChange).bind(this);
+  },
+
+  /**
+   * @return {object}
+   */
+  render: function() {
+  	return (
       <div>
         <Header />
         <MainSection
           allTodos={this.state.todos}
-          areAllComplete={TodoStore.areAllComplete()}
-          />
+          areAllComplete={this.state.areAllComplete}
+        />
         <Footer allTodos={this.state.todos} />
       </div>
-    )
+  	);
+  },
+
+  /**
+   * Event handler for 'change' events coming from the TodoStore
+   */
+  _onChange: function() {
+    this.setState(TodoStore.getState());
   }
-}
+
+});
+
+module.exports = TodoApp;
