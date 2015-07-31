453b55df4ef364be1eb0f57238bc1963bfd86cd2
Andrea Pavoni <andrea.pavoni@gmail.com>
2015-07-31 10:11:49 +0200

    re-add ES6 todomvc example

---
+++ examples/flux-todomvc-es6/js/stores/TodoStore.js
@@ -0,0 +1,74 @@
+import {Store} from 'yafi'
+
+class TodoStore extends Store {
+  constructor() {
+    super()
+
+    this.setInitialState({todos: {}})
+
+    this.bindActions({
+      'TodoActions': {
+        '*': [
+          'create', 'toggleCompleteAll', 'updateText',
+          'destroyCompleted', 'destroy', 'toggleComplete'
+        ]
+      }
+    })
+  }
+
+  areAllComplete() {
+    let todos = this.getState().todos
+    for (let id in todos) {
+      if (!todos[id].complete) { return false }
+    }
+    return true
+  }
+
+  create(text) {
+    // Hand waving here -- not showing how this interacts with XHR or persistent
+    // server-side storage.
+    // Using the current timestamp + random number in place of a real id.
+    let id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36)
+    let todos = this.getState().todos
+    todos[id] = {id: id, complete: false, text: text}
+    this.setState({todos})
+  }
+
+  updateText(todo) {
+    let todos = this.getState().todos
+    todos[todo.id] = Object.assign({}, {text: todo.text})
+    this.setState({todos})
+  }
+
+  toggleCompleteAll() {
+    let todos = this.getState().todos
+    for (let id in todos) {
+      todos[id] = Object.assign({}, {complete: !todos[id].complete})
+    }
+    this.setState({todos})
+  }
+
+  toggleComplete(todo) {
+    let todos = this.getState().todos
+    todos[todo.id] = Object.assign({}, {complete: !todo.complete})
+    this.setState({todos})
+  }
+
+  destroy(id) {
+    let todos = this.getState().todos
+    delete todos[id]
+    this.setState({todos})
+  }
+
+  destroyCompleted() {
+    let todos = this.getState().todos
+    for (let id in todos) {
+      if (todos[id].complete) {
+        delete todos[id]
+      }
+    }
+    this.setState({todos})
+  }
+}
+
+export default new TodoStore()
---
+++ examples/flux-todomvc-es6/js/stores/__tests__/TodoStore-test.js
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2014-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * TodoStore-test
+ */
+
+jest.autoMockOff();
+
+describe('TodoStore', function() {
+  var TodoStore;
+  var actionCreate = {actionType: 'TodoActions.create', payload: 'foo'};
+
+  beforeEach(function() {
+    TodoStore = require('../TodoStore');
+  });
+
+  it('should initialize with no to-do items', function() {
+    var all = TodoStore.getState().todos;
+    expect(all).toEqual({});
+  });
+
+  it('creates a to-do item', function() {
+    TodoStore.dispatcher.dispatch(actionCreate);
+    var all = TodoStore.getState().todos;
+    var keys = Object.keys(all);
+
+    expect(keys.length).toBe(1);
+    expect(all[keys[0]].text).toEqual('foo');
+  });
+
+  it('destroys a to-do item', function() {
+    TodoStore.dispatcher.dispatch(actionCreate);
+    var all = TodoStore.getState().todos;
+    var keys = Object.keys(all);
+
+    expect(keys.length).toBe(1);
+
+    var actionDestroy = {
+      actionType: 'TodoActions.destroy',
+      payload: keys[0]
+    };
+    TodoStore.dispatcher.dispatch(actionDestroy);
+    all = TodoStore.getState().todos;
+    keys = Object.keys(all);
+
+    expect(all[keys[0]]).toBeUndefined();
+  });
+
+  it('can destroy all completed items', function() {
+    var i = 0;
+    for (; i < 2; i++) {
+      TodoStore.dispatcher.dispatch(actionCreate);
+    }
+    var all = TodoStore.getState().todos;
+    var keys = Object.keys(all);
+    expect(keys.length).toBe(2);
+
+    var actionComplete = {
+      actionType: 'TodoActions.toggleComplete',
+      payload: all[keys[0]]
+    };
+    TodoStore.dispatcher.dispatch(actionComplete);
+
+    var actionDestroyCompleted = {
+      actionType: 'TodoActions.destroyCompleted'
+    };
+    TodoStore.dispatcher.dispatch(actionDestroyCompleted);
+    all = TodoStore.getState().todos;
+    keys = Object.keys(all);
+
+    expect(keys.length).toBe(1);
+  });
+
+  it('can determine whether all to-do items are complete', function() {
+    var i = 0;
+    for (; i < 3; i++) {
+      TodoStore.dispatcher.dispatch(actionCreate);
+    }
+    expect(Object.keys(TodoStore.getState().todos).length).toBe(3);
+    expect(TodoStore.areAllComplete()).toBe(false);
+
+    var actionCompleteAll = {
+      actionType: 'TodoActions.toggleCompleteAll'
+    };
+    TodoStore.dispatcher.dispatch(actionCompleteAll);
+    expect(TodoStore.areAllComplete()).toBe(true);
+
+    var all = TodoStore.getState().todos;
+    var keys = Object.keys(all);
+    var actionComplete = {
+      actionType: 'TodoActions.toggleComplete',
+      payload: all[keys[0]]
+    };
+    TodoStore.dispatcher.dispatch(actionComplete);
+    expect(TodoStore.areAllComplete()).toBe(false);
+  });
+});
